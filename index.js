4345a8c64b69e8f30be56ab9bc0549a0adb9d497
Shahed hossen <shahedhossensaad@gmail.com>
2022-07-28 01:19:59 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+
+for (var i = 0; i < 7; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var ButtonSelector = this.innerHTML;
+    MakeSound(ButtonSelector);
+    buttonAnimation(ButtonSelector);
+  });
+}
+document.addEventListener("keydown", function(event)
+{
+  MakeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+function MakeSound(key)
+{
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+
+    default:
+
+  }
+}
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
